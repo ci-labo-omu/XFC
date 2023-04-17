253ad299b95d82a259796d9f7cbbf1a34d758abb
AyatoTomofuji <seb01090@st.osakafu-u.ac.jp>
2023-04-17 18:22:04 +0900

    ランクによる評価 評価値追加に不具合あり

--- examples/train_emo_MF_rank2.py
+++ examples/train_emo_MF_rank2.py
@@ -5,7 +5,6 @@ from deap import creator, base, tools, algorithms
 import matplotlib.pyplot as plt
 from func_emo import selNSGA22
 
-
 if __name__ == "__main__":
     # Instantiate an instance of TrainerEnvironment.
     # The default settings should be sufficient, but check out the documentation for more information
@@ -42,9 +41,8 @@ if __name__ == "__main__":
                 25.6119877, 56.20575568, 85.31037087, 156.41788735, 13.28000091,
                 75.04230663, 145.83883738, -5.34633099, 79.93202705, 170.01952603, ]
 
-    f_list = [(0.0, 0.0) for i in range(50)]
 
-
+    # this is a evaluate function
     def run_2out(gene, gene_out):
         accu, time = 0, 0
         for scene in Scenario_list:
@@ -57,55 +55,66 @@ if __name__ == "__main__":
         return [accu, time]
 
 
-    def feasible(individual1):
-        x1, x2, x3, x4, x5, x6 = individual1
-        return x1 < x2 < x3 < x4 < x5 and -0.2 < x1 < 1.0 and -0.2 < x2 < 1.0 and -0.2 < x3 < 1.0 and -0.2 < x4 < 1.0 and -0.2 < x5 < 1.0 and 0 <= x6 <= 1.0
+    # これは制約関数で，制約を満たすかどうかを判定する
 
-
-    # 制約適応度
-    def constrained_fitness(individual):
-
-        if not feasible(individual):
-            individual.f1 = 0.0
-            individual.f2 = 0.0
-            return 0.0, 0.0
-        else:
-            a = run_2out(individual, best_out)
-            individual.f1, individual.f2 = a
-            print(a)
-            return a[0], a[1]
 
 
     NGEN = 100  # 繰り返し世代数
     MU = 200  # 集団内の個体数
     CXPB = 0.9  # 交叉率
-    """
-    def evaluate_individual_rank(individual):
-        f1, f2 = constrained_fitness(individual)
-        individual.f1 = f1
-        individual.f2 = f2
-        f1_rank = sum(f1 <= f[0] for f in f_list)
-        f2_rank = sum(f2 <= f[1] for f in f_list)
-        return f1_rank, f2_rank
-    """
+
 
     f1_values = np.zeros((MU, 2))
     f2_values = np.zeros((MU, 2))
 
     # make ranking from f1_values, f2_values, sorting each value
-
+    # this is an individual to be evaluated
+    # Individualは実数値のリストであり，適応度は2つの目的関数の値であり，目的関数は2つの評価値の，それぞれにおいての順位である　また，個体はf1, f2の値を属性として持つ
+    # f1, f2は属性であり，それらを評価する関数を作る
+    # それぞれの個体のf1,f2それぞれの順位が評価値となり，それの最小化問題を解く
     creator.create("FitnessMin", base.Fitness, weights=(-1.0, -1.0))
-    creator.create("Individual", list, fitness=creator.FitnessMin, f1=0.0, f2=0.0)
+    creator.create("Individual", list, fitness=creator.FitnessMin)
 
+
+    # クラスIndividualに対し，属性f1, f2を追加して，それをメソッドで値を代入できるようにしてください
 
     class IndividualWithF(creator.Individual):
-        def evaluate_individual_rank(self):
-            f1_values, f2_values = values_pop(pop)
-            self.f1, self.f2 = constrained_fitness(self)
-            return sum(f1 < self.f1 for f1 in f1_values), sum(f2 < self.f2 for f2 in f2_values)
+        f1 = 0.0
+        f2 = 0.0
 
-        def return_rank(individual, f1_values, f2_values):
-            return sum(f1 < individual.f1 for f1 in f1_values), sum(f2 < individual.f2 for f2 in f2_values)
+        def feasible(self):
+            x1, x2, x3, x4, x5, x6 = self
+            return x1 < x2 < x3 < x4 < x5 and -0.2 < x1 < 1.0 and -0.2 < x2 < 1.0 and -0.2 < x3 < 1.0 and -0.2 < x4 < 1.0 and -0.2 < x5 < 1.0 and 0 <= x6 <= 1.0
+
+        # これは制約関数で，制約を満たす場合のみ評価関数を実行する
+        def constrained_fitness(self):
+            if self.feasible():
+                self.f1, self.f2 = run_2out(self, best_out)
+                return self.f1, self.f2
+            else:
+                return 0.0, 0.0
+        def evaluate_individual_rank(self):
+            f1, f2 = self.constrained_fitness()
+            self.f1 = f1
+            self.f2 = f2
+            f1_rank = sum(f1 <= f[0] for f in f_list)
+            f2_rank = sum(f2 <= f[1] for f in f_list)
+            return f1_rank, f2_rank
+        #実際の評価関数は，f1, f2の値を，それぞれの目的関数の値として，それぞれの目的関数の値の順位を返すようにしてください
+        # それぞれの目的関数の値の順位は，f_listの中の値と比較して，それぞれの目的関数の値が，f_listの中の値より小さいものの数を返すようにしてください
+
+        def evaluate_individual(self):
+            f1, f2 = self.constrained_fitness()
+            self.f1 = f1
+            self.f2 = f2
+            return f1, f2
+
+
+    #f_listを作る
+    f_list = []
+    for i in range(MU):
+        f_list.append([f1_values[i, 0], f2_values[i, 0]])
+
 
 
     toolbox = base.Toolbox()
@@ -118,7 +127,6 @@ if __name__ == "__main__":
 
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)
     toolbox.register("evaluate", IndividualWithF.evaluate_individual_rank)
-
     toolbox.register("mate", tools.cxSimulatedBinaryBounded, low=-0.2, up=1.0, eta=20.0)
 
     toolbox.register("mutates", tools.mutPolynomialBounded, up=1, low=-0.2, indpb=1 / 6, eta=30)
@@ -145,6 +153,7 @@ if __name__ == "__main__":
     invalid_ind = [ind for ind in pop if not ind.fitness.valid]
     fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
     for ind, fit in zip(invalid_ind, fitnesses):
+        print(fit)
         ind.fitness.values = fit
     pop = toolbox.select(pop, len(pop))
 
@@ -155,6 +164,12 @@ if __name__ == "__main__":
 
     # 最適計算の実行
     for gen in range(1, NGEN):
+        #ここでf_listを更新する
+        f1_values, f2_values = values_pop(pop)
+        f_list = []
+        for i in range(MU):
+            f_list.append([f1_values[i], f2_values[i]])
+
         # 子母集団生成
         offspring = tools.selTournamentDCD(pop, len(pop))
         offspring = [toolbox.clone(ind) for ind in offspring]
@@ -181,8 +196,10 @@ if __name__ == "__main__":
         pop = toolbox.select(pop + offspring, MU)
         print(f"Generation {gen}:")
         print(logbook.stream)
+        #各世代ごとの個体の評価値を表示，f1,f2も表示する
         for j, ind in enumerate(pop):
-            print("Individual ", j + 1, ": ", ind, "Rank: ", ind.fitness.values, "Fitness: ", ind.fitness.values)
+            print(f"Individual {j + 1}: {ind} Rank: {ind.fitness.values} Fitness: {ind.f1} {ind.f2}")
+
 
         record = stats.compile(pop)
         logbook.record(gen=gen, evals=len(invalid_ind), **record)
@@ -192,6 +209,15 @@ if __name__ == "__main__":
     fitnesses = np.array([list(pop[i].fitness.values) for i in range(len(pop))])
     plt.plot(fitnesses_init[:, 0], fitnesses_init[:, 1], "b.", label="Initial")
     plt.plot(fitnesses[:, 0], fitnesses[:, 1], "r.", label="Optimized")
+    plt.legend(loc="upper right")
+    plt.title("fitnesses")
+    plt.xlabel("f1")
+    plt.ylabel("f2")
+    plt.grid(True)
+    plt.show()
+    #最終的なf1, f2のパレートフロントをプロットする
+    f1_values, f2_values = values_pop(pop)
+    plt.plot(f1_values, f2_values, "r.", label="Optimized")
     plt.legend(loc="upper right")
     plt.title("fitnesses")
     plt.xlabel("f1")
